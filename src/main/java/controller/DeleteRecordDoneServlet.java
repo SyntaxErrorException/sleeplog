7a857f2769da2a94833e8d1847211bcdb7642049
zd2L20 <zd2L20@gmail.com>
2022-08-17 15:42:00 +0900

    ラジオボタンを縦に修正

---
+++ src/main/java/controller/DeleteRecordDoneServlet.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DailyRecordDao;
+import dao.DaoFactory;
+
+/**
+ * Servlet implementation class DeleteRecordDoneServlet
+ */
+@WebServlet("/deleteRecordDone")
+public class DeleteRecordDoneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DailyRecordDao recordDao = DaoFactory.createDailyRecordDao();
+		try {
+			recordDao.delete(Integer.parseInt(request.getParameter("id")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		response.sendRedirect("showRecord");
+	}
+
+}
